9c954939b50d62ab8ddd1838a3478938b2c9d034
BaranBaranDev <auniversty@gmail.com>
2024-06-08 14:56:28 +0300

    Added refresh control and guard in scrollViewDidScroll(_:)

--- MarvelApp/MarvelApp/Scenes/Character/CharacterViewController.swift
+++ MarvelApp/MarvelApp/Scenes/Character/CharacterViewController.swift
@@ -19,6 +19,8 @@ final class CharacterViewController: UIViewController {
     // MARK:  Properties
     private var marvelResults : [Results] = []
     private var isGridView : Bool = false
+    private var isLoadingMoreData: Bool = false
+    
     
     //MARK: Dependencies
     private var interactor: (CharacterDataStore & CharacterBusinessLogic)
@@ -132,7 +134,14 @@ final class CharacterViewController: UIViewController {
         
         // Segmented Control
         navigationItem.titleView = segmentedControl
-        segmentedControl.selectedSegmentIndex = 0 
+        segmentedControl.selectedSegmentIndex = 0
+        
+        
+        // UIRefrestController
+        tableView.refreshControl = UIRefreshControl()
+        tableView.refreshControl?.addTarget(self, action: #selector(refreshData), for: .valueChanged)
+        collectionView.refreshControl = UIRefreshControl()
+        collectionView.refreshControl?.addTarget(self, action: #selector(refreshData), for: .valueChanged)
         
         
     }
@@ -179,17 +188,30 @@ final class CharacterViewController: UIViewController {
         tableView.reloadData()
         collectionView.reloadData()
     }
+    
+    
+    @objc fileprivate func refreshData() {
+        interactor.fetchCharecter(request: CharacterModels.fetchCharacter.Request()) // Verileri yenilemek için API çağrısı
+    }
+    
+    
 }
-
 
 // MARK: - CharacterDisplayLogic
 extension CharacterViewController: CharacterDisplayLogic {
     func display(viewModel: CharacterModels.fetchCharacter.Viewmodel) {
         DispatchQueue.main.async { [weak self] in
             guard let self = self else { return }
-            self.marvelResults = viewModel.characterList
-            tableView.reloadData()
-            collectionView.reloadData()
+            if self.isLoadingMoreData {
+                self.marvelResults.append(contentsOf: viewModel.characterList) // Yeni verileri mevcut listenin sonuna ekler
+                self.isLoadingMoreData = false // Daha fazla veri yükleme işlemi tamamlandı
+            } else {
+                self.marvelResults = viewModel.characterList // Yeni verilerle listeyi tamamen günceller
+            }
+            self.tableView.reloadData() // Tabloyu yeniden yükle
+            self.collectionView.reloadData() // Koleksiyonu yeniden yükle
+            self.tableView.refreshControl?.endRefreshing() // Yenileme işlemini durdur
+            self.collectionView.refreshControl?.endRefreshing() // Yenileme işlemini durdur
         }
     }
 }
@@ -261,7 +283,35 @@ extension CharacterViewController: UICollectionViewDelegate, UICollectionViewDat
     }
 }
 
-
+// MARK: - UIScrollViewDelegate
+extension CharacterViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        // Kaydırma pozisyonunu alır
+        let position = scrollView.contentOffset.y
+        
+        // İçeriğin toplam yüksekliğini alır
+        let contentHeight = scrollView.contentSize.height
+        
+        // ScrollView'ın görünür alanının yüksekliğini alır
+        let scrollViewHeight = scrollView.frame.size.height
+        
+        // Kullanıcı içeriğin sonuna yaklaştı mı kontrol eder
+        if position > (contentHeight - scrollViewHeight - 100) {
+            // Eğer halihazırda veri çekme işlemi yapılıyorsa, fonksiyondan çıkar
+            guard !isLoadingMoreData else { return }
+            
+            // Veri çekme işlemi başlatıldığını işaretle
+            isLoadingMoreData = true
+            
+            // Daha fazla veri çek
+            loadMoreData()
+        }
+    }
+    
+    private func loadMoreData(){
+        interactor.fetchCharecter(request: CharacterModels.fetchCharacter.Request())
+    }
+}
 
 
 // MARK: - UISearchResultsUpdating Protocol
